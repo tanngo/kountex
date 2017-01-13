9ee8ac859d186acdf0a952eb89fafbe8ff36c03c
Nguyen Tan Ngo <ngo.nguyen@evizi.com>
2017-01-13 14:40:14 +0700

    gggg

--- www/KountExample.js
+++ www/KountExample.js
@@ -4,7 +4,7 @@ var KountExample = function (){}
 KountExample.prototype.coolMethod = function(arg0, success, error) {
     exec(success, error, "KountExample", "coolMethod", [arg0]);
 };
-KountExample.prototype.getTextN = function() {
+KountExample.prototype.ReturnText = function() {
     exec(success, error, "KountExample", "ReturnText",[]);
 };
 KountExample.prototype.getNextAppointment = function(success, error, args) {
