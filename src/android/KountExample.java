83712d449dba2215ac905dfdc7ab2f72fd4118b4
Nguyen Tan Ngo <ngo.nguyen@evizi.com>
2017-01-17 12:07:21 +0700

    vvvv

--- src/android/KountExample.java
+++ src/android/KountExample.java
@@ -18,6 +18,15 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import java.util.Locale;
 import android.widget.Toast;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -38,7 +47,7 @@ public class KountExample extends CordovaPlugin {
 	public void ReturnText(String message, CallbackContext callbackContext)
 	{
          Toast toast=Toast.makeText(this, "hien thi tu toast",   Toast.LENGTH_LONG);
-        toast.show();
+         toast.show();
 		 callbackContext.success(message);
 	}
     private void coolMethod(String message, CallbackContext callbackContext) {
