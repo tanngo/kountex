b46f854ac6010c1aecf43ba96cb60a7ad1b0d794
Nguyen Tan Ngo <ngo.nguyen@evizi.com>
2017-01-17 12:32:32 +0700

    bbb

--- src/android/KountExample.java
+++ src/android/KountExample.java
@@ -19,11 +19,7 @@ import android.net.NetworkInfo;
 import java.util.Locale;
 import android.widget.Toast;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
